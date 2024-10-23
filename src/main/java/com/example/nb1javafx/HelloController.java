7677920fe1a3a8526eee8c8172d516ec0df3b90d
pterek47 <terekmate123@gmail.com>
2024-10-23 19:40:23 +0200

    Menu template done

--- src/main/java/com/example/nb1javafx/HelloController.java
+++ src/main/java/com/example/nb1javafx/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.nb1javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -28,4 +29,59 @@ public class HelloController {
     }
 
 
+    public void olvas2Item(ActionEvent actionEvent) {
+        Stage newWindow = new Stage();
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 400);
+
+        Label label = new Label("Hello GAMF!");
+        root.setCenter(label);
+
+        newWindow.setTitle("2. feladat - Olvas2");
+        newWindow.setScene(scene);
+        newWindow.show();
+    }
+
+    public void irItem(ActionEvent actionEvent) {
+        Stage newWindow = new Stage();
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 400);
+
+        Label label = new Label("Hello GAMF!");
+        root.setCenter(label);
+
+        newWindow.setTitle("3. feladat - Ír");
+        newWindow.setScene(scene);
+        newWindow.show();
+    }
+
+    public void modositItem(ActionEvent actionEvent) {
+        Stage newWindow = new Stage();
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 400);
+
+        Label label = new Label("Hello GAMF!");
+        root.setCenter(label);
+
+        newWindow.setTitle("4. feladat - Módosít");
+        newWindow.setScene(scene);
+        newWindow.show();
+    }
+
+    public void torolItem(ActionEvent actionEvent) {
+        Stage newWindow = new Stage();
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 400);
+
+        Label label = new Label("Hello GAMF!");
+        root.setCenter(label);
+
+        newWindow.setTitle("5. feladat - Töröl");
+        newWindow.setScene(scene);
+        newWindow.show();
+    }
 }
