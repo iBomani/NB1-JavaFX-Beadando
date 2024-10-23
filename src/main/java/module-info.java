738e5d8575a122ea597f40ac132ea24f51994aa3
iBomani <robertkosztandi3013@gmail.com>
2024-10-23 20:09:06 +0200

    generate soap client files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.nb1javafx {
     requires javafx.fxml;
     requires javafx.web;
 
+    requires java.xml.bind;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -11,6 +12,8 @@ module com.example.nb1javafx {
     requires com.almasb.fxgl.all;
     requires java.persistence;
     requires java.sql;
+    requires java.jws;
+    requires java.xml.ws;
 
     opens com.example.nb1javafx to javafx.fxml;
     exports com.example.nb1javafx;
