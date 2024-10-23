3e1eb03de6c0d59b772affa945a86887b3645db3
iBomani <robertkosztandi3013@gmail.com>
2024-10-23 19:08:03 +0200

    rework template and menu

--- src/main/java/com/example/nb1javafx/HelloApplication.java
+++ src/main/java/com/example/nb1javafx/HelloApplication.java
@@ -1,86 +1,22 @@
 package com.example.nb1javafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage primaryStage) {
-        MenuBar menuBar = new MenuBar();
-
-
-        // ADATBAZIS MENU
-        Menu adatbazisMenu = new Menu("Adatbázis");
-
-        MenuItem olvasItem = new MenuItem("Olvas");
-        olvasItem.setOnAction(event -> olvasItem());
-
-        MenuItem olvas2Item = new MenuItem("Olvas2");
-        MenuItem irItem = new MenuItem("Ír");
-        MenuItem modositItem = new MenuItem("Módosít");
-        MenuItem torolItem = new MenuItem("Töröl");
-
-        adatbazisMenu.getItems().addAll(olvasItem, olvas2Item, irItem, modositItem, torolItem);
-
-        menuBar.getMenus().add(adatbazisMenu);
-
-        // SOAPKLIENS MENU
-        Menu soapKliensMenu = new Menu("SoapKliens");
-
-        MenuItem letoltesItem = new MenuItem("Letöltés");
-        MenuItem letoltes2Item = new MenuItem("Letöltés2");
-        MenuItem grafikonItem = new MenuItem("Grafikon");
-
-        soapKliensMenu.getItems().addAll(letoltesItem, letoltes2Item, grafikonItem);
-
-        menuBar.getMenus().add(soapKliensMenu);
-
-        // PARHUZAMOS MENU
-        Menu parhuzamosMenu = new Menu("Párhuzamos");
-
-        menuBar.getMenus().add(parhuzamosMenu);
-
-        // FOREX MENU
-        Menu forexMenu = new Menu("Forex");
-
-        MenuItem szamlaItem = new MenuItem("Számlainformációk");
-        MenuItem arakItem = new MenuItem("Aktuális árak");
-        MenuItem histArakItem = new MenuItem("Historikus árak");
-        MenuItem pozicioNyitasItem = new MenuItem("Pozíció nyitás");
-        MenuItem pozicioZarasItem = new MenuItem("Pozíció zárás");
-        MenuItem nyitottPozItem = new MenuItem("Nyitott pozíciók");
-
-        forexMenu.getItems().addAll(szamlaItem, arakItem, histArakItem, pozicioNyitasItem, pozicioZarasItem, nyitottPozItem);
-
-        menuBar.getMenus().add(forexMenu);
-
-        BorderPane root = new BorderPane();
-        root.setTop(menuBar);
+    public void start(Stage primaryStage) throws Exception {
+        // FXML betöltése
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
 
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("JavaFX Beadandó - Terék Péter & Kosztándi Róbert");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    private void olvasItem() {
-        Stage newWindow = new Stage();
-
-
-        BorderPane root = new BorderPane();
-
-        Scene scene = new Scene(root, 600, 400);
-
-
-        newWindow.setTitle("1. feladat - Olvas");
-        newWindow.setScene(scene);
-
-        newWindow.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/nb1javafx/HelloController.java
+++ src/main/java/com/example/nb1javafx/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.nb1javafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem olvasItem;
+
+    @FXML
+    public void olvasItem() {
+        Stage newWindow = new Stage();
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 400);
+
+        Label label = new Label("Hello GAMF!");
+        root.setCenter(label);
+
+        newWindow.setTitle("1. feladat - Olvas");
+        newWindow.setScene(scene);
+        newWindow.show();
     }
+
+
 }
