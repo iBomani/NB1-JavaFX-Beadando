bc6c505de5abb4c2f4860c06ba3bb4f40cde4726
iBomani <robertkosztandi3013@gmail.com>
2024-10-23 18:51:46 +0200

    menubar and template

--- src/main/java/com/example/nb1javafx/HelloApplication.java
+++ src/main/java/com/example/nb1javafx/HelloApplication.java
@@ -14,28 +14,73 @@ public class HelloApplication extends Application {
         MenuBar menuBar = new MenuBar();
 
 
-        Menu adatbazisMenu = new Menu("WE CAN GO AURA FOR AURA");
-
+        // ADATBAZIS MENU
+        Menu adatbazisMenu = new Menu("Adatbázis");
 
         MenuItem olvasItem = new MenuItem("Olvas");
+        olvasItem.setOnAction(event -> olvasItem());
+
         MenuItem olvas2Item = new MenuItem("Olvas2");
         MenuItem irItem = new MenuItem("Ír");
         MenuItem modositItem = new MenuItem("Módosít");
         MenuItem torolItem = new MenuItem("Töröl");
 
-
         adatbazisMenu.getItems().addAll(olvasItem, olvas2Item, irItem, modositItem, torolItem);
 
-
         menuBar.getMenus().add(adatbazisMenu);
+
+        // SOAPKLIENS MENU
+        Menu soapKliensMenu = new Menu("SoapKliens");
+
+        MenuItem letoltesItem = new MenuItem("Letöltés");
+        MenuItem letoltes2Item = new MenuItem("Letöltés2");
+        MenuItem grafikonItem = new MenuItem("Grafikon");
+
+        soapKliensMenu.getItems().addAll(letoltesItem, letoltes2Item, grafikonItem);
+
+        menuBar.getMenus().add(soapKliensMenu);
+
+        // PARHUZAMOS MENU
+        Menu parhuzamosMenu = new Menu("Párhuzamos");
+
+        menuBar.getMenus().add(parhuzamosMenu);
+
+        // FOREX MENU
+        Menu forexMenu = new Menu("Forex");
+
+        MenuItem szamlaItem = new MenuItem("Számlainformációk");
+        MenuItem arakItem = new MenuItem("Aktuális árak");
+        MenuItem histArakItem = new MenuItem("Historikus árak");
+        MenuItem pozicioNyitasItem = new MenuItem("Pozíció nyitás");
+        MenuItem pozicioZarasItem = new MenuItem("Pozíció zárás");
+        MenuItem nyitottPozItem = new MenuItem("Nyitott pozíciók");
+
+        forexMenu.getItems().addAll(szamlaItem, arakItem, histArakItem, pozicioNyitasItem, pozicioZarasItem, nyitottPozItem);
+
+        menuBar.getMenus().add(forexMenu);
 
         BorderPane root = new BorderPane();
         root.setTop(menuBar);
 
         Scene scene = new Scene(root, 600, 400);
-        primaryStage.setTitle("JavaFX CRUD alkalmazás");
+        primaryStage.setTitle("JavaFX Beadandó - Terék Péter & Kosztándi Róbert");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void olvasItem() {
+        Stage newWindow = new Stage();
+
+
+        BorderPane root = new BorderPane();
+
+        Scene scene = new Scene(root, 600, 400);
+
+
+        newWindow.setTitle("1. feladat - Olvas");
+        newWindow.setScene(scene);
+
+        newWindow.show();
     }
 
     public static void main(String[] args) {
