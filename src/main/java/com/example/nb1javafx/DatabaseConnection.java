6381a7086567aa8d525e61c8310c0bdf464f77fa
pterek47 <terekmate123@gmail.com>
2024-11-05 00:09:43 +0100

    db rename

--- src/main/java/com/example/nb1javafx/DatabaseConnection.java
+++ src/main/java/com/example/nb1javafx/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     public static Connection connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:../NB-1.db";
+            String url = "jdbc:sqlite:../adatok.db";
             conn = DriverManager.getConnection(url);
 
         } catch (SQLException e) {
